bfa4d53c82d5c3f7b47dde0778e99315f7198bac
Chris B <1029chris@gmail.com>
2015-11-23 13:03:14 -0800

    Trapping player on island

--- main.lua
+++ main.lua
@@ -43,10 +43,14 @@ function love.load()
   island.x = island.x/2
   island.y = island.y/2
   block = {}
-  block.body = love.physics.newBody(world, island.x, island.y, "static")
-  block.shape = love.physics.newRectangleShape(48, 48)
-  block.fixture = love.physics.newFixture(block.body, block.shape, 50)
-  block.body:setLinearDamping(1)
+  block.body1 = love.physics.newBody(world, island.x/2-48, island.y/2+48, "static")
+  block.shape1 = love.physics.newRectangleShape(78, 32)
+  block.fixture1 = love.physics.newFixture(block.body1, block.shape1, 50)
+  block.body1:setLinearDamping(1)
+  block.body2 = love.physics.newBody(world, island.x/2-48, island.y/2+48+48+48+12, "static")
+  block.shape2 = love.physics.newRectangleShape(78, 32)
+  block.fixture2 = love.physics.newFixture(block.body2, block.shape2, 50)
+  block.body2:setLinearDamping(1)
   block.x = 0
   block.y = 0
   grass = {}
@@ -60,8 +64,8 @@ function love.update(dt)
   world:update(dt)
   player.x = player.body:getX()
   player.y = player.body:getY()
-  block.x = block.body:getX()
-  block.y = block.body:getY()
+  block.x = block.body1:getX()
+  block.y = block.body1:getY()
   if love.keyboard.isDown("up") and love.keyboard.isDown("down", "left", "right") == false and player.cutscene == false then
     player.keyboard = "up"
   elseif love.keyboard.isDown("down") and love.keyboard.isDown("left", "up", "right") == false and player.cutscene == false then
@@ -165,6 +169,6 @@ end
 function love.draw()
   love.graphics.draw(island.img, island.x, island.y, 0, 3, 3, 80, 32)
   love.graphics.draw(shadowimg, player.x, player.y, 0, 3, 3, 0, -10)
-  love.graphics.draw(grass.img, grass.grassdirt, block.x, block.y, 0, 3, 3)
+  --love.graphics.draw(grass.img, grass.grassdirt, block.x, block.y, 0, 3, 3)
   love.graphics.draw(player.img, player.sprite, player.x, player.y, 0, 3, 3, -2, 12)
 end
