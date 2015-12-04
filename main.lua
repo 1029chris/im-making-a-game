60f89535e8fce8edd213626db47d3c83e6a37560
Chris B <1029chris@gmail.com>
2015-12-03 16:10:49 -0800

    Reduced code for animations

--- main.lua
+++ main.lua
@@ -13,23 +13,23 @@ function love.load()
   player.x = 0
   player.y = 0
   player.move = false
-  player.front1 = love.graphics.newQuad(2, 1, 12, 26, assets.player:getDimensions())
-  player.front2 = love.graphics.newQuad(18, 1, 12, 26, assets.player:getDimensions())
-  player.front3 = love.graphics.newQuad(34, 1, 12, 26, assets.player:getDimensions())
-  player.back1 = love.graphics.newQuad(2, 29, 12, 26, assets.player:getDimensions())
-  player.back2 = love.graphics.newQuad(18, 29, 12, 26, assets.player:getDimensions())
-  player.back3 = love.graphics.newQuad(34, 29, 12, 26, assets.player:getDimensions())
-  player.right1 = love.graphics.newQuad(2, 57, 12, 26, assets.player:getDimensions())
-  player.right2 = love.graphics.newQuad(18, 57, 12, 26, assets.player:getDimensions())
-  player.right3 = love.graphics.newQuad(34, 57, 12, 26, assets.player:getDimensions())
-  player.left1 = love.graphics.newQuad(2, 85, 12, 26, assets.player:getDimensions())
-  player.left2 = love.graphics.newQuad(18, 85, 12, 26, assets.player:getDimensions())
-  player.left3 = love.graphics.newQuad(34, 85, 12, 26, assets.player:getDimensions())
-  player.sprite = player.front1
-  player.front = player.front1
-  player.back = player.back1
-  player.right = player.right1
-  player.left = player.left1
+  player.front = {}
+  player.back = {}
+  player.right = {}
+  player.left = {}
+  player.front[1] = love.graphics.newQuad(2, 1, 12, 26, assets.player:getDimensions())
+  player.front[2] = love.graphics.newQuad(18, 1, 12, 26, assets.player:getDimensions())
+  player.front[3] = love.graphics.newQuad(34, 1, 12, 26, assets.player:getDimensions())
+  player.back[1] = love.graphics.newQuad(2, 29, 12, 26, assets.player:getDimensions())
+  player.back[2] = love.graphics.newQuad(18, 29, 12, 26, assets.player:getDimensions())
+  player.back[3] = love.graphics.newQuad(34, 29, 12, 26, assets.player:getDimensions())
+  player.right[1] = love.graphics.newQuad(2, 57, 12, 26, assets.player:getDimensions())
+  player.right[2] = love.graphics.newQuad(18, 57, 12, 26, assets.player:getDimensions())
+  player.right[3] = love.graphics.newQuad(34, 57, 12, 26, assets.player:getDimensions())
+  player.left[1] = love.graphics.newQuad(2, 85, 12, 26, assets.player:getDimensions())
+  player.left[2] = love.graphics.newQuad(18, 85, 12, 26, assets.player:getDimensions())
+  player.left[3] = love.graphics.newQuad(34, 85, 12, 26, assets.player:getDimensions())
+  player.sprite = player.front[1]
   player.ani = 0
   player.anidelay = 0.2
   player.speed = 120
@@ -39,6 +39,7 @@ function love.load()
   player.body = love.physics.newBody(world, 325, 325, "dynamic")
   player.shape = love.physics.newRectangleShape(36, 36)
   player.fixture = love.physics.newFixture(player.body, player.shape, 1)
+  player.frame = 1
   island = {}
   island.x, island.y = love.graphics.getDimensions()
   island.x = island.x/2
@@ -111,35 +112,35 @@ function love.update(dt)
   end
   if player.keyboard == "up" and player.stop == false then
     player.body:setLinearVelocity(0, -player.speed)
-    player.sprite = player.back
+    player.sprite = player.back[player.frame]
     player.move = true
   elseif player.keyboard == "down" and player.stop == false then
     player.body:setLinearVelocity(0, player.speed)
-    player.sprite = player.front
+    player.sprite = player.front[player.frame]
     player.move = true
   elseif player.keyboard == "left" and player.stop == false then
     player.body:setLinearVelocity(-player.speed, 0)
-    player.sprite = player.left
+    player.sprite = player.left[player.frame]
     player.move = true
   elseif player.keyboard == "right" and player.stop == false then
     player.body:setLinearVelocity(player.speed, 0)
-    player.sprite = player.right
+    player.sprite = player.right[player.frame]
     player.move = true
   elseif player.keyboard == "rightdown" and player.stop == false then
     player.body:setLinearVelocity(player.speed, player.speed)
-    player.sprite = player.front
+    player.sprite = player.front[player.frame]
     player.move = true
   elseif player.keyboard == "rightup" and player.stop == false then
     player.body:setLinearVelocity(player.speed, -player.speed)
-    player.sprite = player.back
+    player.sprite = player.back[player.frame]
     player.move = true
   elseif player.keyboard == "leftdown" and player.stop == false then
     player.body:setLinearVelocity(-player.speed, player.speed)
-    player.sprite = player.front
+    player.sprite = player.front[player.frame]
     player.move = true
   elseif player.keyboard == "leftup" and player.stop == false then
     player.body:setLinearVelocity(-player.speed, -player.speed)
-    player.sprite = player.back
+    player.sprite = player.back[player.frame]
     player.move = true
   elseif player.keyboard == "updown" and player.stop == false then
     player.body:setLinearVelocity(0, 0)
@@ -150,34 +151,21 @@ function love.update(dt)
   elseif love.keyboard.isDown("right", "left", "up", "down") == false then
     player.move = false
     player.body:setLinearVelocity(0, 0)
+  elseif player.keyboard == "none" then
+    player.frame = 1
   end
   player.ani = player.ani + dt
   if player.move == true and player.ani < player.anidelay then
-    player.front = player.front2
-    player.back = player.back2
-    player.right = player.right2
-    player.left = player.left2
+    player.frame = 2
   elseif player.ani >= player.anidelay and player.move == true and player.ani < player.anidelay * 2 then
-    player.front = player.front1
-    player.back = player.back1
-    player.right = player.right1
-    player.left = player.left1
+    player.frame = 1
   elseif player.ani >= player.anidelay * 2 and player.move == true and player.ani < player.anidelay * 3 then
-    player.front = player.front3
-    player.back = player.back3
-    player.right = player.right3
-    player.left = player.left3
+    player.frame = 3
   elseif player.ani >= player.anidelay * 3 and player.move == true then
-    player.front = player.front1
-    player.back = player.back1
-    player.right = player.right1
-    player.left = player.left1
+    player.frame = 1
   elseif player.move == false then
     player.ani = 0
-    player.front = player.front1
-    player.back = player.back1
-    player.right = player.right1
-    player.left = player.left1
+    player.frame = 1
   else
     player.move = false
   end
