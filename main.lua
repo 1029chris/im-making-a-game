d4efcc42f1a2fc54ec8feccd16052d442b7e842b
Chris B <1029chris@gmail.com>
2015-12-03 16:22:43 -0800

    changed tilemap back to grass.

--- main.lua
+++ main.lua
@@ -75,10 +75,14 @@ function love.load()
   test.body = love.physics.newBody(world, -32, -48)
   test.shape = love.physics.newChainShape(false, 130, 145, 510, 145, 510, 190, 560, 190, 560, 290, 510, 290)
   test.fixture = love.physics.newFixture(test.body, test.shape, 1)
+  grass = {}
+  grass.grass = love.graphics.newQuad(0, 0, 16, 16, assets.grass:getDimensions())
+  grass.dirt = love.graphics.newQuad(0, 16, 32, 16, assets.grass:getDimensions())
+  grass.grassdirt = love.graphics.newQuad(0, 16, 16, 16, assets.grass:getDimensions())
   tilemap = {}
-  tilemap.grass = love.graphics.newQuad(0, 0, 16, 16, assets.grass:getDimensions())
-  tilemap.dirt = love.graphics.newQuad(0, 16, 32, 16, assets.grass:getDimensions())
-  tilemap.grassdirt = love.graphics.newQuad(0, 16, 16, 16, assets.grass:getDimensions())
+  tilemap[1] = grass.grass
+  tilemap[2] = grass.grassdirt
+  tilemap[3] = grass.dirt
 end
 function love.update(dt)
   world:update(dt)
@@ -174,14 +178,14 @@ function love.update(dt)
   end
 end
 function love.draw()
-  if island.mapfinish == false then
+  while island.mapfinish == false do
     if island.data[island.tile] == 1 then
       love.graphics.setCanvas(island.map)
-        love.graphics.draw(assets.grass, tilemap.grass, island.tilex, island.tiley)
+        love.graphics.draw(assets.grass, grass.grass, island.tilex, island.tiley)
       love.graphics.setCanvas()
     elseif island.data[island.tile] == 2 then
       love.graphics.setCanvas(island.map)
-        love.graphics.draw(assets.grass, tilemap.grassdirt, island.tilex, island.tiley)
+        love.graphics.draw(assets.grass, grass.grassdirt, island.tilex, island.tiley)
       love.graphics.setCanvas()
     end
     if island.data[island.tile] == 9998 then
