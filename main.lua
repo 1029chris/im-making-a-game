446594f4c16a0e2e431b4c299943181f96332093
Chris B <1029chris@gmail.com>
2015-12-16 12:05:50 -0800

    Moved player code to player.lua
    I also changed player.keyboard to system.keyboard

--- main.lua
+++ main.lua
@@ -1,44 +1,18 @@
 function love.load()
+  require("player")
   love.physics.setMeter(32)
   world = love.physics.newWorld(0, 0, true)
   love.graphics.setBackgroundColor(102, 204, 255)
   love.graphics.setDefaultFilter("nearest")
+  system = {
+    keyboard = "none"
+  }
   assets = {
     player = love.graphics.newImage("assets/char.png"),
     shadow = love.graphics.newImage("assets/shadow.png"),
     grass = love.graphics.newImage("assets/dirt.png")
   }
-  player = {}
-  player.x = 0
-  player.y = 0
-  player.move = false
-  player.front = {}
-  player.back = {}
-  player.right = {}
-  player.left = {}
-  player.front[1] = love.graphics.newQuad(2, 1, 12, 26, assets.player:getDimensions())
-  player.front[2] = love.graphics.newQuad(18, 1, 12, 26, assets.player:getDimensions())
-  player.front[3] = love.graphics.newQuad(34, 1, 12, 26, assets.player:getDimensions())
-  player.back[1] = love.graphics.newQuad(2, 29, 12, 26, assets.player:getDimensions())
-  player.back[2] = love.graphics.newQuad(18, 29, 12, 26, assets.player:getDimensions())
-  player.back[3] = love.graphics.newQuad(34, 29, 12, 26, assets.player:getDimensions())
-  player.right[1] = love.graphics.newQuad(2, 57, 12, 26, assets.player:getDimensions())
-  player.right[2] = love.graphics.newQuad(18, 57, 12, 26, assets.player:getDimensions())
-  player.right[3] = love.graphics.newQuad(34, 57, 12, 26, assets.player:getDimensions())
-  player.left[1] = love.graphics.newQuad(2, 85, 12, 26, assets.player:getDimensions())
-  player.left[2] = love.graphics.newQuad(18, 85, 12, 26, assets.player:getDimensions())
-  player.left[3] = love.graphics.newQuad(34, 85, 12, 26, assets.player:getDimensions())
-  player.sprite = player.front[1]
-  player.ani = 0
-  player.anidelay = 0.2
-  player.speed = 120
-  player.keyboard = "none"
-  player.stop = false
-  player.cutscene = false
-  player.body = love.physics.newBody(world, 325, 220, "dynamic")
-  player.shape = love.physics.newRectangleShape(36, 36)
-  player.fixture = love.physics.newFixture(player.body, player.shape, 1)
-  player.frame = 1
+  player.load()
   map = {
     island = {
       x = love.graphics.getWidth()/2,
@@ -84,93 +58,33 @@ function love.load()
 end
 function love.update(dt)
   world:update(dt)
-  player.x = player.body:getX()
-  player.y = player.body:getY()
   wall.x = wall.body1:getX()
   wall.y = wall.body1:getY()
   if love.keyboard.isDown("right") and love.keyboard.isDown("down") and player.cutscene == false then
-    player.keyboard = "rightdown"
+    system.keyboard = "rightdown"
   elseif love.keyboard.isDown("right") and love.keyboard.isDown("up") and player.cutscene == false then
-    player.keyboard = "rightup"
+    system.keyboard = "rightup"
   elseif love.keyboard.isDown("left") and love.keyboard.isDown("down") and player.cutscene == false then
-    player.keyboard = "leftdown"
+    system.keyboard = "leftdown"
   elseif love.keyboard.isDown("left") and love.keyboard.isDown("up") and player.cutscene == false then
-    player.keyboard = "leftup"
+    system.keyboard = "leftup"
   elseif love.keyboard.isDown("up") and love.keyboard.isDown("down") and player.cutscene == false then
-    player.keyboard = "updown"
+    system.keyboard = "updown"
   elseif love.keyboard.isDown("left") and love.keyboard.isDown("right") and player.cutscene == false then
-    player.keyboard = "leftright"
+    system.keyboard = "leftright"
   elseif love.keyboard.isDown("up") and player.cutscene == false then
-    player.keyboard = "up"
+    system.keyboard = "up"
   elseif love.keyboard.isDown("down") and player.cutscene == false then
-    player.keyboard = "down"
+    system.keyboard = "down"
   elseif love.keyboard.isDown("left") and player.cutscene == false then
-    player.keyboard = "left"
+    system.keyboard = "left"
   elseif love.keyboard.isDown("right") and player.cutscene == false then
-    player.keyboard = "right"
+    system.keyboard = "right"
   elseif player.cutscene == true then
   else
-    player.keyboard = "none"
+    system.keyboard = "none"
   end
-  if player.keyboard == "up" and player.stop == false then
-    player.body:setLinearVelocity(0, -player.speed)
-    player.sprite = player.back[player.frame]
-    player.move = true
-  elseif player.keyboard == "down" and player.stop == false then
-    player.body:setLinearVelocity(0, player.speed)
-    player.sprite = player.front[player.frame]
-    player.move = true
-  elseif player.keyboard == "left" and player.stop == false then
-    player.body:setLinearVelocity(-player.speed, 0)
-    player.sprite = player.left[player.frame]
-    player.move = true
-  elseif player.keyboard == "right" and player.stop == false then
-    player.body:setLinearVelocity(player.speed, 0)
-    player.sprite = player.right[player.frame]
-    player.move = true
-  elseif player.keyboard == "rightdown" and player.stop == false then
-    player.body:setLinearVelocity(player.speed, player.speed)
-    player.sprite = player.front[player.frame]
-    player.move = true
-  elseif player.keyboard == "rightup" and player.stop == false then
-    player.body:setLinearVelocity(player.speed, -player.speed)
-    player.sprite = player.back[player.frame]
-    player.move = true
-  elseif player.keyboard == "leftdown" and player.stop == false then
-    player.body:setLinearVelocity(-player.speed, player.speed)
-    player.sprite = player.front[player.frame]
-    player.move = true
-  elseif player.keyboard == "leftup" and player.stop == false then
-    player.body:setLinearVelocity(-player.speed, -player.speed)
-    player.sprite = player.back[player.frame]
-    player.move = true
-  elseif player.keyboard == "updown" and player.stop == false then
-    player.body:setLinearVelocity(0, 0)
-    player.move = false
-  elseif player.keyboard == "leftright" and player.stop == false then
-    player.body:setLinearVelocity(0, 0)
-    player.move = false
-  elseif love.keyboard.isDown("right", "left", "up", "down") == false then
-    player.move = false
-    player.body:setLinearVelocity(0, 0)
-  elseif player.keyboard == "none" then
-    player.frame = 1
-  end
-  player.ani = player.ani + dt
-  if player.move == true and player.ani < player.anidelay then
-    player.frame = 2
-  elseif player.ani >= player.anidelay and player.move == true and player.ani < player.anidelay * 2 then
-    player.frame = 1
-  elseif player.ani >= player.anidelay * 2 and player.move == true and player.ani < player.anidelay * 3 then
-    player.frame = 3
-  elseif player.ani >= player.anidelay * 3 and player.move == true then
-    player.frame = 1
-  elseif player.move == false then
-    player.ani = 0
-    player.frame = 1
-  else
-    player.move = false
-  end
+  player.update(dt)
   if player.ani >= player.anidelay * 4 then
     player.ani = 0
   end
@@ -193,8 +107,7 @@ function love.draw()
     map.tile = map.tile + 1
   end
   love.graphics.draw(map.current.map, map.current.x, map.current.y, 0, 3, 3, 80, 32)
-  love.graphics.draw(assets.shadow, player.x, player.y, 0, 3, 3, 0, -10)
   --love.graphics.draw(assets.grass, grass.grassdirt, wall.x, wall.y, 0, 3, 3)
-  love.graphics.draw(assets.player, player.sprite, player.x, player.y, 0, 3, 3, -2, 12)
+  player.draw()
   love.graphics.print(love.mouse.getX() .. ", " .. love.mouse.getY())
 end
---
+++ player.lua
@@ -0,0 +1,99 @@
+player = {}
+function player.load()
+  player.x = 0
+  player.y = 0
+  player.move = false
+  player.front = {}
+  player.back = {}
+  player.right = {}
+  player.left = {}
+  player.front[1] = love.graphics.newQuad(2, 1, 12, 26, assets.player:getDimensions())
+  player.front[2] = love.graphics.newQuad(18, 1, 12, 26, assets.player:getDimensions())
+  player.front[3] = love.graphics.newQuad(34, 1, 12, 26, assets.player:getDimensions())
+  player.back[1] = love.graphics.newQuad(2, 29, 12, 26, assets.player:getDimensions())
+  player.back[2] = love.graphics.newQuad(18, 29, 12, 26, assets.player:getDimensions())
+  player.back[3] = love.graphics.newQuad(34, 29, 12, 26, assets.player:getDimensions())
+  player.right[1] = love.graphics.newQuad(2, 57, 12, 26, assets.player:getDimensions())
+  player.right[2] = love.graphics.newQuad(18, 57, 12, 26, assets.player:getDimensions())
+  player.right[3] = love.graphics.newQuad(34, 57, 12, 26, assets.player:getDimensions())
+  player.left[1] = love.graphics.newQuad(2, 85, 12, 26, assets.player:getDimensions())
+  player.left[2] = love.graphics.newQuad(18, 85, 12, 26, assets.player:getDimensions())
+  player.left[3] = love.graphics.newQuad(34, 85, 12, 26, assets.player:getDimensions())
+  player.sprite = player.front[1]
+  player.ani = 0
+  player.anidelay = 0.2
+  player.speed = 120
+  player.stop = false
+  player.cutscene = false
+  player.body = love.physics.newBody(world, 325, 220, "dynamic")
+  player.shape = love.physics.newRectangleShape(36, 36)
+  player.fixture = love.physics.newFixture(player.body, player.shape, 1)
+  player.frame = 1
+end
+function player.update(dt)
+  player.x = player.body:getX()
+  player.y = player.body:getY()
+  if system.keyboard == "up" and player.stop == false then
+    player.body:setLinearVelocity(0, -player.speed)
+    player.sprite = player.back[player.frame]
+    player.move = true
+  elseif system.keyboard == "down" and player.stop == false then
+    player.body:setLinearVelocity(0, player.speed)
+    player.sprite = player.front[player.frame]
+    player.move = true
+  elseif system.keyboard == "left" and player.stop == false then
+    player.body:setLinearVelocity(-player.speed, 0)
+    player.sprite = player.left[player.frame]
+    player.move = true
+  elseif system.keyboard == "right" and player.stop == false then
+    player.body:setLinearVelocity(player.speed, 0)
+    player.sprite = player.right[player.frame]
+    player.move = true
+  elseif system.keyboard == "rightdown" and player.stop == false then
+    player.body:setLinearVelocity(player.speed, player.speed)
+    player.sprite = player.front[player.frame]
+    player.move = true
+  elseif system.keyboard == "rightup" and player.stop == false then
+    player.body:setLinearVelocity(player.speed, -player.speed)
+    player.sprite = player.back[player.frame]
+    player.move = true
+  elseif system.keyboard == "leftdown" and player.stop == false then
+    player.body:setLinearVelocity(-player.speed, player.speed)
+    player.sprite = player.front[player.frame]
+    player.move = true
+  elseif system.keyboard == "leftup" and player.stop == false then
+    player.body:setLinearVelocity(-player.speed, -player.speed)
+    player.sprite = player.back[player.frame]
+    player.move = true
+  elseif system.keyboard == "updown" and player.stop == false then
+    player.body:setLinearVelocity(0, 0)
+    player.move = false
+  elseif system.keyboard == "leftright" and player.stop == false then
+    player.body:setLinearVelocity(0, 0)
+    player.move = false
+  elseif love.keyboard.isDown("right", "left", "up", "down") == false then
+    player.move = false
+    player.body:setLinearVelocity(0, 0)
+  elseif system.keyboard == "none" then
+    player.frame = 1
+  end
+  player.ani = player.ani + dt
+  if player.move == true and player.ani < player.anidelay then
+    player.frame = 2
+  elseif player.ani >= player.anidelay and player.move == true and player.ani < player.anidelay * 2 then
+    player.frame = 1
+  elseif player.ani >= player.anidelay * 2 and player.move == true and player.ani < player.anidelay * 3 then
+    player.frame = 3
+  elseif player.ani >= player.anidelay * 3 and player.move == true then
+    player.frame = 1
+  elseif player.move == false then
+    player.ani = 0
+    player.frame = 1
+  else
+    player.move = false
+  end
+end
+function player.draw()
+  love.graphics.draw(assets.shadow, player.x, player.y, 0, 3, 3, 0, -10)
+  love.graphics.draw(assets.player, player.sprite, player.x, player.y, 0, 3, 3, -2, 12)
+end
