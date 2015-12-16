d309c28d0741f05f2d87c6294d89b0c571ed0094
Chris B <1029chris@gmail.com>
2015-12-16 12:46:01 -0800

    Moved map code to maps.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 function love.load()
   require("player")
+  require("maps")
   love.physics.setMeter(32)
   world = love.physics.newWorld(0, 0, true)
   love.graphics.setBackgroundColor(102, 204, 255)
@@ -12,26 +13,12 @@ function love.load()
     shadow = love.graphics.newImage("assets/shadow.png"),
     grass = love.graphics.newImage("assets/dirt.png")
   }
+  grass = {}
+  grass.grass = love.graphics.newQuad(0, 0, 16, 16, assets.grass:getDimensions())
+  grass.dirt = love.graphics.newQuad(0, 16, 32, 16, assets.grass:getDimensions())
+  grass.grassdirt = love.graphics.newQuad(0, 16, 16, 16, assets.grass:getDimensions())
   player.load()
-  map = {
-    island = {
-      x = love.graphics.getWidth()/2,
-      y = love.graphics.getHeight()/2,
-      data = {
-        0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 9999,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9999,
-        2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 9999,
-        0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 9998
-      },
-      map = love.graphics.newSpriteBatch(assets.grass, 1000),
-    },
-    finish = false,
-    tile = 1,
-    tilex = 0,
-    tiley = 0,
-    current = {}
-  }
-  map.current = map.island
+  maps.load()
   wall = {}
   wall.body1 = love.physics.newBody(world, map.island.x/2-48-40, map.island.y/2+48-40, "static")
   wall.shape1 = love.physics.newRectangleShape(78, 32)
@@ -51,10 +38,6 @@ function love.load()
   test.body = love.physics.newBody(world, -32, -48)
   test.shape = love.physics.newChainShape(false, 130, 145, 510, 145, 510, 190, 560, 190, 560, 290, 510, 290)
   test.fixture = love.physics.newFixture(test.body, test.shape, 1)
-  grass = {}
-  grass.grass = love.graphics.newQuad(0, 0, 16, 16, assets.grass:getDimensions())
-  grass.dirt = love.graphics.newQuad(0, 16, 32, 16, assets.grass:getDimensions())
-  grass.grassdirt = love.graphics.newQuad(0, 16, 16, 16, assets.grass:getDimensions())
 end
 function love.update(dt)
   world:update(dt)
@@ -90,22 +73,7 @@ function love.update(dt)
   end
 end
 function love.draw()
-  while map.finish == false do
-    if map.current.data[map.tile] == 1 then
-      map.current.map:add(grass.grass, map.tilex, map.tiley)
-    elseif map.current.data[map.tile] == 2 then
-      map.current.map:add(grass.grassdirt, map.tilex, map.tiley)
-    end
-    if map.current.data[map.tile] == 9998 then
-      map.finish = true
-    elseif map.current.data[map.tile] ~= 9999 then
-        map.tilex = map.tilex + 16
-    elseif map.current.data[map.tile] == 9999 then
-        map.tilex = 0
-        map.tiley = map.tiley + 16
-    end
-    map.tile = map.tile + 1
-  end
+  maps.draw()
   love.graphics.draw(map.current.map, map.current.x, map.current.y, 0, 3, 3, 80, 32)
   --love.graphics.draw(assets.grass, grass.grassdirt, wall.x, wall.y, 0, 3, 3)
   player.draw()
---
+++ maps.lua
@@ -0,0 +1,41 @@
+maps = {}
+function maps.load()
+  map = {}
+  map.island = {}
+  map.island.x = love.graphics.getWidth()/2
+  map.island.y = love.graphics.getHeight()/2
+  map.island.data = {
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 9999,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9999,
+    2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 9999,
+    0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 9998
+  }
+  map.island.map = love.graphics.newSpriteBatch(assets.grass, 1000)
+  map.finish = false
+  map.tile = 1
+  map.tilex = 0
+  map.tiley = 0
+  map.current = {}
+  map.current = map.island
+end
+function maps.update()
+
+end
+function maps.draw()
+  while map.finish == false do
+    if map.current.data[map.tile] == 1 then
+      map.current.map:add(grass.grass, map.tilex, map.tiley)
+    elseif map.current.data[map.tile] == 2 then
+      map.current.map:add(grass.grassdirt, map.tilex, map.tiley)
+    end
+    if map.current.data[map.tile] == 9998 then
+      map.finish = true
+    elseif map.current.data[map.tile] ~= 9999 then
+        map.tilex = map.tilex + 16
+    elseif map.current.data[map.tile] == 9999 then
+        map.tilex = 0
+        map.tiley = map.tiley + 16
+    end
+    map.tile = map.tile + 1
+  end
+end
